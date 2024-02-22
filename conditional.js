f23bf7efa46e009980547015e4f776561082a1b0
rohit kc <103559078+rohitkarki9980@users.noreply.github.com>
2024-02-22 08:29:08 +0530

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,59 @@
+// //conditional statement
+// // if(condition){
+// //     statement 1 will excute
+// // }
+// // else{
+
+// // }
+
+
+// let a = 5;
+
+// if(a < 0){
+//     console.log("Negative");
+// }
+// else if{
+//      statement2
+// }
+// else if{
+
+// }
+// else{
+//     console.log("Positive")
+// }
+
+// switch(key){
+//     case value1:statement1;
+//     break;
+//     case value2:statment2;
+//     break;
+//     case value3:statement3;
+//     break
+//     default:
+//         if no value matches;
+// }
+// //nested if-else statement
+// if(condition 1){
+//     if (condition3){
+
+//     }
+//     else{
+
+//     }
+// }
+// else{
+//     if (condition 2)(
+        
+//     )
+//     else{
+
+//     }
+// }
+
+// //compound statements
+// if(condition 1 && condition2 && condition 3 && ...conditionN ){
+
+// }
+// else{
+
+// }
